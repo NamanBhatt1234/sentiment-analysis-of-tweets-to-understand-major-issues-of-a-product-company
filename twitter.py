866cdab7f595d6dca23f4f7bf6b7bbeac2e5ed73
Naman Bhatt <65487375+NamanBhatt1234@users.noreply.github.com>
2022-06-27 11:07:55 +0530

    Add files via upload
    Authentication of twiiter api done using twiiter developer account .Key used in twitter.py module.

---
+++ twitter.py
@@ -0,0 +1,22 @@
+import tweepy
+import pandas as pd
+
+def get_twitter_text(s):
+
+    api_key = ''
+    api_key_secret = ''
+    access_token = ''
+    access_token_secret = ''
+
+    auth = tweepy.OAuthHandler(api_key, api_key_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    keywords = '#'+s+'-filter:retweets'
+    tweets=tweepy.Cursor(api.search_tweets,q=keywords,lang="en",tweet_mode="extended").items(1000)
+    columns = ['Tweet']
+    data = []
+    for tweet in tweets:
+        data.append([ tweet.full_text])
+    df = pd.DataFrame(data, columns=columns)
+    #print(df)
+    return df
